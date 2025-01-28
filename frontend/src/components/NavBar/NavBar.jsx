73e081a2390af59ea2f723a042f4155cefebff4f
Your Name <danagabay@msn.com>
2025-01-27 19:10:07 -0500

    Search bar created for nav

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -4,14 +4,39 @@ import { InputText } from 'primereact/inputtext';
 import { Menubar } from 'primereact/menubar';
 import { Badge } from 'primereact/badge';
 import styled from 'styled-components';
+import { useContext, useState } from 'react';
+import { SearchContext } from '../../searchContext';
 // import './NavBar.css';
 
 const CustomNavBar = styled(Menubar)`
   height: 100px;
 `;
 
-export default function NavBar({ user, setUser }) {
+export default function NavBar({ user, setUser, setSearch }) {
+  const search = useContext(SearchContext);
+  const [searchVal, setSearchVal] = useState(search);
+
   const navigate = useNavigate();
+
+  const handleInputChange = (e) => {
+    setSearchVal(e.target.value); // updates the search bar state with the current
+    // of the input field
+    if (e.target.value === '') {
+      // explicitly sets searchVal to an empty string ('')
+      setSearchVal(''); // even though the first line already sets it to '',
+      // this line ensures the state is updated correctly and may act as a safeguard.
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      // checks if the key pressed is the Enter key, if it is that means
+      // the user wants to preform a search
+      setSearch(searchVal); // this updates the search state, the value used for
+      // this search is taken from searchVal which shows the users current input in the
+      // search bar.
+    }
+  };
 
   function handleLogOut() {
     logOut();
@@ -65,23 +90,27 @@ export default function NavBar({ user, setUser }) {
     },
   ];
 
-  const start = (
-    <img
-      alt='logo'
-      src='https://primefaces.org/cdn/primereact/images/logo.png'
-      height='40'
-      className='mr-2'
-    ></img>
-  );
+  // const start = (            <------- use later for LEGODEX logo
+  //   <img
+  //     alt='logo'
+  //     src='https://primefaces.org/cdn/primereact/images/logo.png'
+  //     height='40'
+  //     className='mr-2'
+  //   ></img>
+  // );
 
   const end = (
     <div className='align-items-center gap-2' style={{ display: 'flex' }}>
       <InputText
         placeholder='Search...'
+        value={searchVal}
+        onChange={handleInputChange}
+        onKeyDown={handleKeyDown}
         type='text'
         className='w-8rem sm:w-auto'
         style={{ marginRight: 5 }}
       />
+
       {user ? (
         <>
           <span className='p-menuitem-link'>
